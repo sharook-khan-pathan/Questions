19a8e2a690578f4c86936203a66db433bc26744e
sharookkhanpathan626@gmail.com <sharookkhanpathan626@gmail.com>
2025-08-29 10:29:20 +0530

    Add files

---
+++ src/Collections/Task7.java
@@ -0,0 +1,18 @@
+package Collections;
+import java.util.*;
+public class Task7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector<Double> v = new Vector<Double>();
+		v.add(10.76);
+		v.add(46.47);
+		v.add(45.71);
+		System.out.println(v);
+		v.remove(1);
+		System.out.println(v);
+		
+		
+	}
+
+}
