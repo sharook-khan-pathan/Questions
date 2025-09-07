d732fe578349efcd80cc1e48c1fea64b9f8dd9d4
sharookkhanpathan626@gmail.com <sharookkhanpathan626@gmail.com>
2025-09-07 21:44:46 +0530

    Add Files

--- src/Problems/Task6.java
+++ src/Problems/Task6.java
@@ -1,10 +1,10 @@
 package Problems;
-
+import java.util.*;
 public class Task6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
 	}
 
 }
